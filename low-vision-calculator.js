7c18f09772090b924b35ba26edbbf0e081186a8e
Clark Phillips <clark.r.phillips@gmail.com>
2015-06-24 07:43:46 -0500

    - named all the boxes  - added jquery  - box11 and box12 are currently added and their sum is displayed when you click submit

--- low-vision-calculator.js
+++ low-vision-calculator.js
@@ -1,5 +1,23 @@
 function computeVision() {
-  alert("Hey Matt");
+  var box11 = parseInt( $("#box11").val() );
+  var box12 = parseInt( $("#box12").val() );
+  var box13 = parseInt( $("#box13").val() );
+  var box14 = parseInt( $("#box14").val() );
 
-  // UPDATE THE FIELD FROm <label for="vision">UPDATE ME</label>
+  var box21 = parseInt( $("#box21").val() );
+  var box22 = parseInt( $("#box22").val() );
+  var box23 = parseInt( $("#box23").val() );
+  var box24 = parseInt( $("#box24").val() );
+
+  var box31 = parseInt( $("#box31").val() ) ;
+  var box32 = parseInt( $("#box32").val() );
+  var box33 = parseInt( $("#box33").val() );
+  var box34 = parseInt( $("#box34").val() );
+
+  var box41 = parseInt( $("#box41").val() );
+  var box42 = parseInt( $("#box42").val() );
+  var box43 = parseInt( $("#box43").val() );
+  var box44 = parseInt( $("#box44").val() );
+
+  $("#vision").text(box11 + box12);
 }
