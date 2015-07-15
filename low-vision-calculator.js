832318a95086dfa504756e0e8a6aa5abf20605d5
Clark Phillips <clark.phillips@oracle.com>
2015-07-15 15:06:16 -0500

    update EVD whenever you update magnification

--- low-vision-calculator.js
+++ low-vision-calculator.js
@@ -26,25 +26,19 @@ function computeVision(boxChanged) {
   var NMOS = box32/box42;
   var NTOD = box33/box43;
   var NTOS = box34/box44;
-  
-  
-  
-  
 
-  
   console.log(box11 + " / " + box21 + " = " + box11 / box21);
   console.log(DMOD);
   console.log(DMOS);
-  
 
   var magnification =  (Math.max(DTOD, DTOS)) / Math.max(DMOD, DMOS);
   $("#magnification").val(magnification);
+  $("#evd").val($("#box31").val() / magnification)
+
   $("#box31").val(box11 / 100);
   $("#box32").val(box12 / 100);
   $("#box41").val(box21 / 100);
   $("#box42").val(box22 / 100);
-  
-  
 
   // This will update the arbitrary div at the bottom of everything
   // $("#vision").text(magnification);
@@ -58,7 +52,11 @@ function considerDisabling(idThatChanged) {
   //    the submit button to be able to do something
   // 3. enable the submit button (allow to be clicked if all the inputs are present)
   // 4. disable the submit button if any of the inputs are deleted
-   
+
   //$("#box31").prop("disabled", true);
   //$("#" + idThatChanged).prop("disabled", true);
+}
+
+function calculateEVD(magnification) {
+  $("#evd").val($("#box31").val() / magnification)
 }
