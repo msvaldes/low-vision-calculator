43c224c1c93f28bf338369396b1c7ee7d9fdeced
Clark Phillips <clark.phillips@oracle.com>
2015-07-21 09:46:43 -0500

    added fixed width and improved readability

--- low-vision-calculator.js
+++ low-vision-calculator.js
@@ -30,86 +30,78 @@ function computeVision(boxChanged) {
   console.log(box11 + " / " + box21 + " = " + box11 / box21);
   console.log(DMOD);
   console.log(DMOS);
- 
-//distance magnifcation
+
+  //distance magnifcation
   var magnification =  (Math.max(DTOD, DTOS)) / Math.max(DMOD, DMOS);
   $("#magnification").val(magnification);
-  
-  
-  
-  
-  
 
   //Near fields population based on distance inputs
-  if($("#box31").val()===''){
-  $("#box31").val($("#box11").val() / $("#box11").val()*0.4);}
-  else{$("#box31").val();}
-    
-  if($("#box32").val()===''){
-  $("#box32").val($("#box12").val() / $("#box12").val()*0.4);}
-  else{$("#box32").val();}
-      
-  if($("#box41").val()===''){
-  $("#box41").val($("#box21").val()/100 * 0.4/$("#box11").val()*100);}
-  else{$("#box41").val();}
-  
-  if($("#box42").val()===''){
-  $("#box42").val($("#box22").val()/100 * 0.4/$("#box12").val()*100);}
-  else{$("#box42").val();}
-  
-  
- 
+  if($("#box31").val()==='') {
+    $("#box31").val($("#box11").val() / $("#box11").val()*0.4);
+  } else {
+    $("#box31").val();
+  }
+
+  if($("#box32").val()==='') {
+    $("#box32").val($("#box12").val() / $("#box12").val()*0.4);
+  } else {
+    $("#box32").val();
+  }
+
+  if($("#box41").val()==='') {
+    $("#box41").val($("#box21").val()/100 * 0.4/$("#box11").val()*100);
+  } else {
+    $("#box41").val();
+  }
+
+  if($("#box42").val()==='') {
+    $("#box42").val($("#box22").val()/100 * 0.4/$("#box12").val()*100);
+  } else {
+    $("#box42").val();
+  }
+
   //Near field target OD and OS
   $("#box33").val($("#box31").val()*$("#box43").val()/$("#box41").val());
   $("#box34").val($("#box32").val()*$("#box44").val()/$("#box42").val());
 
-  
   //Near Magnification
   var magnificationNear =  (Math.min($("#box41").val()/$("#box43").val(),$("#box42").val()/$("#box44").val() )) ;
   $("#magnificationNear").val(magnificationNear);
-  
+
   //EVD calcuation
   $("#evd").val($("#box31").val()/magnification);
   $("#evdNear").val(Math.max($("#box33").val(),$("#box34").val()));
-  
-  
+
   //EVP calcuation
   $("#evpNear").val((1/$("#evdNear").val()));
 
   // This will update the arbitrary div at the bottom of everything
   // $("#vision").text(magnification);
-  
-  
+
   // Effective ADD power
   $("#powerAdd").val($("#evpNear").val());
-  
-  
+
   // Effective ADD working distance
   $("#wdAdd").val(1/$("#powerAdd").val());
-  
+
   //IHHM calculations
   $("#powerIHHM").val($("#evpNear").val());
-  
+
   // Effective ADD working distance
   $("#wdIHHM").val(1/$("#powerAdd").val());
-  
+
   //ISM Power Calculation
-var powerTop =  ($("#evpNear").val()- $("#addISM").val());
-var powerBottom = (1- ($("#addISM").val()*$("#wdISM").val()));
-var powerISMcalc = powerTop/powerBottom;
-$("#powerISM").val(Math.ceil(powerISMcalc));
-  }
-
-
+  var powerTop =  ($("#evpNear").val()- $("#addISM").val());
+  var powerBottom = (1- ($("#addISM").val()*$("#wdISM").val()));
+  var powerISMcalc = powerTop/powerBottom;
+  $("#powerISM").val(Math.ceil(powerISMcalc));
+}
 
 function calculateEVD(magnification) {
   $("#evd").val(Math.max($("#box31").val(), $("#box32").val()) / magnification);
-  
-  
-   
-    
-  
-  function considerDisabling(idThatChanged) {
+}
+
+function considerDisabling(idThatChanged) {
   console.log("The ID that changed:", idThatChanged);
   // 1. disable the submit button (you would not be able to click it initially)
   // 2. this function is called whenever a box is changed...
@@ -121,9 +113,7 @@ function calculateEVD(magnification) {
   //$("#box31").prop("disabled", true);
   //$("#" + idThatChanged).prop("disabled", true);
 }
-}
 
 function resetForm(){
   document.getElementbyId("myForm").reset();
-  
 }
